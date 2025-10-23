c6f1e9c49bb32b572d0163d435513457f3b11cb6
millbcc <mill.bcc@gmail.com>
2025-10-23 10:42:39 +0700

    final

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,16 @@
 import express from "express";
 import mysql from "mysql2";
 import cors from "cors";
-import dotenv from "dotenv";
 
-dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST || "localhost",
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "123456",
-  database: process.env.DB_DATABASE || "numerapi"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
 });
 
 app.get("/api/bisection", (req, res) => {
